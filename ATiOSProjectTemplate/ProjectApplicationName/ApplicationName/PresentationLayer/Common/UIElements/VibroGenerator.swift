286e89cf6f4605a99050a6fd365787e109b08271
Anton Tekutov <Anton.Tekutow@yandex.ru>
2021-05-20 23:47:27 +0300

    AlertPresentationViewController + module template; templates script updated.

---
+++ ATiOSProjectTemplate/ProjectApplicationName/ApplicationName/PresentationLayer/Common/UIElements/VibroGenerator.swift
@@ -0,0 +1,14 @@
+//
+//  VibroGenerator.swift
+//  ApplicationName
+//
+//  Created by Антон Текутов on 20.05.2021.
+//
+
+import UIKit
+
+class VibroGenerator {
+    
+    static let light = UIImpactFeedbackGenerator(style: .light)
+    static let heavy = UIImpactFeedbackGenerator(style: .heavy)
+}
