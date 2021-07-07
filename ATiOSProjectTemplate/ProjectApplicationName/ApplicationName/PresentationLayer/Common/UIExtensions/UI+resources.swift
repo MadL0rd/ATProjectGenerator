e9d62e543147ef915d63932de1af70c30527c90d
Anton Tekutov <anton.tekutow@yandex.ru>
2021-07-07 07:50:40 +0300

    UI+resources

---
+++ ATiOSProjectTemplate/ProjectApplicationName/ApplicationName/PresentationLayer/Common/UIExtensions/UI+resources.swift
@@ -0,0 +1,33 @@
+//
+//  UIColor+resources.swift
+//  WhyNFT
+//
+//  Created by Антон Текутов on 12.06.2021.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static var res = R.color.self
+}
+
+extension String {
+    
+    static var res = R.string.localizable.self
+}
+
+extension UIImage {
+    
+    static var res = R.image.self
+}
+
+extension UIFont {
+    
+    static var res = R.font.self
+}
+
+extension CGFloat {
+    
+    static var res = UIConstants.self
+}
