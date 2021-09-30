ef251d846af589537369a593b31adb7823878270
Anton Tekutov <anton.tekutow@yandex.ru>
2021-09-30 04:04:30 +0300

    Template update: request content type

---
+++ ATiOSProjectTemplate/ProjectApplicationName/ApplicationName/CoreLayer/NetworkService/Entities/RequestContentType.swift
@@ -0,0 +1,11 @@
+//
+//  RequestContentType.swift
+//  ApplicationName
+//
+//  Created by Антон Текутов on 31.08.2021.
+//
+
+enum RequestContentType: String {
+    case json = "application/json"
+    case formData = "multipart/form-data"
+}
